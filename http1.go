086124292cb7c69d2ec7d9c5e8a6ec5f626128c7
ArturToikka <artur.toikka@outlook.com>
2017-07-16 17:50:25 +0300

    myName

--- http1.go
+++ http1.go
@@ -6,7 +6,13 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "helloWorld.html")
 }
 
+func handleMyName(w http.ResponseWriter, r *http.Request) {
+	myName := r.FormValue("myName")
+	w.Write([]byte("My name is: "+ myName))
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/myName", handleMyName)
 	http.ListenAndServe(":8000", nil)
 }
